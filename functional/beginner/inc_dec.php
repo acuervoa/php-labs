be55cd871860a1b2c9afe68afba61664c743d010
Andrés Cuervo <holas@andrescuervo.es>
2021-09-29 21:56:36 +0200

    chapter 3

---
+++ functional/beginner/inc_dec.php
@@ -0,0 +1,16 @@
+<?php
+
+function inc($number)
+{
+	$number++;
+	return $number;
+}
+
+function dec($number)
+{
+	$number--;
+	return $number;
+}
+
+var_dump(inc(3));
+var_dump(dec(3));
---
+++ functional/beginner/sums2.php
@@ -0,0 +1,25 @@
+<?php
+
+include('compose.php');
+
+function double($number)
+{
+	return $number * 2;
+}
+function negate($number)
+{
+	return -$number;
+}
+function add_two($number)
+{
+	return $number + 2;
+}
+
+$mango_temp = compose(
+	'double',
+	'negate',
+	'add_two'
+);
+
+echo $mango_temp(6) . "ºC\n\n    ";
+print_r($mango_temp);
